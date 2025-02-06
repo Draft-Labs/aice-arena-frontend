3342cfb099c8f8cf997044acc2d5ad4c72998f39
0xBrian <brian@draftlabs.app>
2025-02-06 17:43:09 -0500

    Update Web3Context.jsx

--- src/context/Web3Context.jsx
+++ src/context/Web3Context.jsx
@@ -8,15 +8,15 @@ import PokerJSON from '../contracts/Poker.json';
 const Web3Context = createContext();
 
 const AVALANCHE_FUJI_CONFIG = {
-  chainId: '0xA869', // 43113 in hex
-  chainName: 'Avalanche Fuji Testnet',
+  chainId: '43113', // 43113 in hex
+  chainName: 'Avalanche Testnet C-Chain',
   nativeCurrency: {
     name: 'AVAX',
     symbol: 'AVAX',
     decimals: 18
   },
   rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
-  blockExplorerUrls: ['https://testnet.snowtrace.io/']
+  blockExplorerUrls: ['https://subnets-test.avax.network/']
 };
 
 export function Web3Provider({ children }) {
