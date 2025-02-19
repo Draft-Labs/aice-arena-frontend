b2595d9efe925d0acb5fd7fcde72424f49160a11
0xBrian <brian@draftlabs.app>
2025-02-19 11:01:09 -0500

    cleanup

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -217,7 +217,7 @@ function Account() {
         } catch (err) {
           console.error('Error updating balance:', err);
         }
-      }, 5000); // Update every 5 seconds
+      }, 5000);
     }
 
     return () => {
