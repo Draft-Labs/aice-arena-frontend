777c7132c5735ddca06e1c693a3e30991ec45e76
0xBrian <brian@draftlabs.app>
2025-02-20 16:22:32 -0500

    changed fuji network details

--- src/config/networks.js
+++ src/config/networks.js
@@ -1,11 +1,11 @@
 export const FUJI_CONFIG = {
   chainId: '0xA869', // 43113 in hex
-  chainName: 'Avalanche Fuji Testnet',
+  chainName: 'Avalanche Testnet C-Chain',
   nativeCurrency: {
     name: 'AVAX',
     symbol: 'AVAX',
     decimals: 18
   },
-  rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
-  blockExplorerUrls: ['https://testnet.snowtrace.io/']
+  rpcUrls: ['​https://api.avax-test.network/ext/bc/C/rpc'],
+  blockExplorerUrls: ['https://subnets-test.avax.network/']
 }; 
